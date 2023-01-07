d6bf274537a223c35a26ea6e39bcef9b7d90a8b2
Matias Alonso <matiasalosno88@gmail.com>
2023-01-07 15:34:04 -0300

    proyecto finalizado det

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <h5>AVE  -  Test 1  -  Desarrollado por Matias Alonso Pisani  -  Enero 2023</h5>  
+      <h5>AVE  -  Test 1 - FrontEnd  -  Desarrollado por Matias Alonso Pisani  -  Enero 2023</h5>  
     </div>
   )
 }
